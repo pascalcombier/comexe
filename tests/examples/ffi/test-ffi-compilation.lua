18a06f48f47db34e597908bbfd55061316acd7e0
Pascal COMBIER <103032158+pascalcombier@users.noreply.github.com>
2026-08-08 09:07:05 +0800

    Improve compilation test

--- tests/examples/ffi/test-ffi-compilation.lua
+++ tests/examples/ffi/test-ffi-compilation.lua
@@ -3,6 +3,7 @@ local Runtime = require("com.runtime")
 local format          = string.format
 local getparam        = Runtime.getparam
 local getrelativepath = Runtime.getrelativepath
+local executecommand  = Runtime.executecommand
 
 local LuaExe = getparam("LUA-EXE")
 
@@ -14,12 +15,14 @@ local FFI_FILES = {
 
 for Index, Filename in ipairs(FFI_FILES) do
   local FilePath = getrelativepath(Filename)
-  local Command  = format([[%s -x --preprocess "%s"]], LuaExe, FilePath)
-  local Success, Reason, ExitCode = os.execute(Command)
+  local Command  = format([["%s" -x --preprocess "%s"]], LuaExe, FilePath)
+  local ExitCode, ExitReason, ResultStdout, ResultStderr = executecommand(Command, nil, "string")
   if (ExitCode == 0) then
     print(format(" OK %s", Filename))
   else
-    print(format("ERR %s preprocessing failed with exit code %d", Filename, ExitCode))
+    print(format("ERR %s preprocessing failed with exit code %q", Filename, ExitCode))
+    print(ResultStderr)
+    print(ResultStdout)
     os.exit(1)
   end
 end
