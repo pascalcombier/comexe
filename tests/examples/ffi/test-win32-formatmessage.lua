f02023757ca8a6a4c927fbec570b824a30251588
Pascal COMBIER <103032158+pascalcombier@users.noreply.github.com>
2026-08-15 08:18:03 +0800

    Add win32 formatmessage test

---
+++ tests/examples/ffi/test-win32-formatmessage.lua
@@ -0,0 +1,24 @@
+local libffi = require("com.ffi")
+local win32  = require("com.win32")
+
+-- @BEGIN FfiDeclarations("BindLibrary", "libffi")
+-- int CopyFileA(void *Source, void *Target, int FailIfExists);
+-- @OUTPUT
+-- Functions
+local CopyFileA
+-- Binding function
+local function BindLibrary (Library)
+  CopyFileA = Library:bind(libffi.sint32, "CopyFileA", libffi.pointer, libffi.pointer, libffi.sint32)
+end
+-- @END
+
+local Kernel32 = libffi.loadlib("kernel32.dll")
+BindLibrary(Kernel32)
+
+if (CopyFileA("Z:\\this-file-does-not-exist.txt", "C:\\target.txt", 0) == 0) then
+  local ErrorCode = win32.getlasterror()
+  print(win32.formatmessage(ErrorCode))
+  print("OK")
+else
+  error("CopyFileA should have failed")
+end
